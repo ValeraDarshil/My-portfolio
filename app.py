a11b301987a2ce4d1b74b58c2a79138d50daf522
valeradarshil <darshilvalera@gmail.com>
2025-07-19 23:21:40 +0530

    Initial commit

--- app.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="My Portfolio", layout="wide")
-
-st.title("Hi, I'm Darshil 👋")
-st.write("Welcome to my portfolio website built with Streamlit!")
